f3624aeda545a37a3ccde8651137ad652496e0cf
Marat Saidov <msaidov@sbdagroup.com>
2021-12-15 22:05:35 +0300

    added process for UnsupervisedBaseline

--- detection/unsupervised_baseline.py
+++ detection/unsupervised_baseline.py
@@ -8,25 +8,53 @@ from detection.models.detectors import SimpleDetector
 
 class UnsupervisedBaseline:
     """
+    TODO
     """
     def __init__(self, sample: str = "topk"):
         if sample not in ["topk", "nucl"]:
             raise ValueError("Wrong value for sample")
         self.detector = SimpleDetector()
-        self.sample = sample
+        self.sample = sample  # TODO: расширить sample
+        self.unsupervised_df = None
 
-    def process(self, df: pd.DataFrame, lm_params: Optional[Dict[str, Any]] = None) -> pd.DataFrame:
+    @staticmethod
+    def process(df: pd.DataFrame, lm_params: Optional[Dict[str, Any]] = None) -> pd.DataFrame:
+        """
+        Parameters
+        ----------
+        df: pd.DataFrame
+            TODO
+        lm_params: Optional[Dict[str, Any]], default=None
+            TODO
+
+        Returns
+        -------
+            TODO
+        """
+        unsupervised_data = []
         for i, row in df.iterrows():
             paragraph = row["text"]
             if not check_paragraph(paragraph):
                 continue
             # TODO-LM: делать через батч
             generated_paragraph = generate_LM([paragraph], lm_params=lm_params)
-        return result
+            unsupervised_data.extend([
+                {
+                    "text": generated_paragraph,
+                    "target": "machine"
+                },
+                {
+                    "text": paragraph,
+                    "target": "human"
+                }
+            ])
+        unsupervised_df = pd.DataFrame(data=unsupervised_data, columns=["paragraph", "target"])
+        return unsupervised_df
 
-    def fit(self, df: pd.DataFrame):
-        df_targets = self.process(df)
-        X, y = df_targets["text"], df_targets["target"]
+    def fit(self, df: pd.DataFrame, force: bool = False):
+        if (not self.unsupervised_df) or (self.unsupervised_df and force):
+            self.unsupervised_df = self.process(df)
+        X, y = self.unsupervised_df["text"], self.unsupervised_df["target"]
         self.detector.fit(X, y)
 
     def predict(self, X_test: pd.DataFrame):
--- tests/test_language_model.py
+++ tests/test_language_model.py
@@ -6,20 +6,35 @@ from detection.models.language_model import LanguageModel
 from detection.models.smr.core import SuffixArray
 
 
-class TestSuperMaximalRepeat(TestCase):
-    def test_super_maximal_repeat(self) -> None:
-        smr_data = [
-            ("", ""),
-            ("abc xx one two_three abc abc-three", "three"),
-            ("kkkkkkk", "kkkkkk"),
+class TestUnsupervisedBaselineTools(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.paragraphs = [
+            "",
+            "One sent. Two sent. C. D. E. F.",
+            "abc xx one. two_three abc abc-three.",
+            "kkkkkkk"
+        ]
+        cls.smr_targets = [
+            "",
+            " sent. ",
+            "three",
+            "kkkkkk"
+        ]
+        cls.prefixes = [
+            "",
+            "One sent. Two sent.",
+            "abc xx one. two_three abc abc-three.",
+            "kkkkkkk."
         ]
 
-        for paragraph, repeat in smr_data:
+    def test_super_maximal_repeat(self) -> None:
+        for paragraph, repeat in zip(self.paragraphs, self.smr_targets):
             assert_that(super_maximal_repeat(paragraph), equal_to(repeat))
 
-
-def test_retrieve_prefix():
-    pass
+    def test_retrieve_prefix(self) -> None:
+        for paragraph, prefix in zip(self.paragraphs, self.prefixes):
+            assert_that(retrieve_prefix(paragraph), equal_to(prefix))
 
 
 def test_deterministic_output():
