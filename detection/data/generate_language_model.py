07f09f359ff66384bc748a95acd683521a3413a2
Marat Saidov <msaidov@sbdagroup.com>
2021-12-18 15:07:36 +0300

    added semi-supervised setting template

--- detection/data/generate_language_model.py
+++ detection/data/generate_language_model.py
@@ -1,14 +1,26 @@
 from typing import Any, Dict, List, Optional
 
-from detection.models.smr.core import SuffixArray
+from detection.models.const import (
+    LM_LENGTH_LOWER_BOUND,
+    LM_LENGTH_UPPER_BOUND
+)
 from detection.models.language_model import LanguageModel
+from detection.models.smr.core import SuffixArray
 
 
-def check_paragraph(paragraph: str) -> bool:
+def check_input_paragraph(paragraph: str) -> bool:
     """
     TODO: понять, где вызывать предикат
     """
-    return len(paragraph) >= 100
+    return len(paragraph) >= LM_LENGTH_LOWER_BOUND
+
+
+def check_output_paragraph(paragraph: str) -> bool:
+    return len(paragraph) >= LM_LENGTH_UPPER_BOUND
+
+
+def trim_output_paragraph(paragraph: str) -> str:
+    return paragraph[:LM_LENGTH_UPPER_BOUND]
 
 
 def retrieve_prefix(paragraph: str, sentence_num: int = 2) -> str:
--- detection/unsupervised_baseline.py
+++ detection/unsupervised_baseline.py
@@ -2,7 +2,11 @@ from typing import Any, Dict, Optional
 
 import pandas as pd
 
-from detection.data.generate_language_model import check_paragraph, generate_language_model, retrieve_prefix
+from detection.data.generate_language_model import (
+    check_input_paragraph, check_output_paragraph, generate_language_model, retrieve_prefix,
+    super_maximal_repeat, trim_output_paragraph
+)
+from detection.models.const import SEMI_SUPERVISED_HUMAN_RATE
 from detection.models.detectors import SimpleDetector
 
 
@@ -14,7 +18,7 @@ class UnsupervisedBaseline:
         if sample not in ["topk", "nucl"]:
             raise ValueError("Wrong value for sample")
         self.detector = SimpleDetector()
-        self.sample = sample  # TODO: расширить sample
+        self.sample = sample  # TODO: расширить sample: top_k = 10, p = 0.8
         self.unsupervised_df = None
 
     @staticmethod
@@ -33,25 +37,36 @@ class UnsupervisedBaseline:
         """
         unsupervised_data = []
         paragraphs = df.values.reshape(-1,).tolist()
-        paragraphs = list(filter(check_paragraph, paragraphs))
+        paragraphs = list(filter(check_input_paragraph, paragraphs))
         generated_paragraphs = generate_language_model(paragraphs, lm_params=lm_params)
         for i, generated_paragraph in enumerate(generated_paragraphs):
-            unsupervised_data.extend([
-                {
-                    "text": generated_paragraph,
-                    "target": "machine"
-                },
-                {
-                    "text": paragraphs[i],
-                    "target": "human"
-                }
-            ])
+            if check_output_paragraph(generated_paragraph):
+                unsupervised_data.extend([
+                    {
+                        "text": trim_output_paragraph(generated_paragraph),
+                        "target": "machine"
+                    },
+                    {
+                        "text": paragraphs[i],
+                        "target": "human"
+                    }
+                ])
         unsupervised_df = pd.DataFrame(data=unsupervised_data, columns=["text", "target"])
         return unsupervised_df
 
+    def semi_supervise(self, df: pd.DataFrame) -> pd.DataFrame:
+        supervised_sample = df.head(SEMI_SUPERVISED_HUMAN_RATE * len(df))
+        supervised_sample = supervised_sample[supervised_sample["target"] == "human"]
+        semi_supervised_df = df.tail((1.0 - SEMI_SUPERVISED_HUMAN_RATE) * len(df))
+        semi_supervised_df["repeats"] = semi_supervised_df["text"].apply(super_maximal_repeat)
+        # TODO: сэмплируем, выбираем таргеты
+        # TODO: написать тест, что разметка проходит корректно
+        return semi_supervised_df
+
     def fit(self, df: pd.DataFrame, force: bool = False):
         if (not self.unsupervised_df) or (self.unsupervised_df and force):
-            self.unsupervised_df = self.process(df)
+            df = self.process(df)
+            self.unsupervised_df = self.semi_supervise(df)
         X, y = self.unsupervised_df["text"], self.unsupervised_df["target"]
         self.detector.fit(X, y)
 
