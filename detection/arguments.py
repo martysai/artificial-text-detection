c3c148c443ac082de2661a7edbeee824d6a2f7fe
Marat Saidov <msaidov@sbdagroup.com>
2021-12-21 20:08:28 +0300

    added multilingual option support

--- detection/arguments.py
+++ detection/arguments.py
@@ -29,6 +29,7 @@ def set_args(parser: argparse.ArgumentParser):
     train_args.add_argument("--easy_nmt_batch_size", type=int, default=16)
     train_args.add_argument("--easy_nmt_model_name", type=str, default="opus-mt")
     train_args.add_argument("--easy_nmt_offline", type=bool, default=False)
+    train_args.add_argument("--multilingual", type=bool, default=False)
     # TODO-EasyNMT: improve for many models
     train_args.add_argument(
         "--offline_prefix",
