10c77e435502fc1a5203d465dc6e124a8678c08a
Marat Saidov <msaidov@sbdagroup.com>
2022-01-14 11:48:37 +0300

    separated semi-supervised and unsupervised options

--- detection/unsupervised_baseline.py
+++ detection/unsupervised_baseline.py
@@ -21,12 +21,17 @@ class UnsupervisedBaseline:
     """
     TODO
     """
-    def __init__(self, args: argparse.Namespace, sample: str = "topk"):
+    def __init__(
+            self, args: argparse.Namespace, labeled_df: pd.DataFrame = None, mode: str = "unsupervised", sample: str = "topk"
+    ):
         if sample not in ["topk", "nucl"]:
             raise ValueError("Wrong value for sample")
         self.detector = SimpleDetector(args=args)
+        self.labeled_df = labeled_df
+        if mode not in ["semi-supervised", "unsupervised"]:
+            raise ValueError("Possible mode options are 'semi-supervised' and 'unsupervised'")
+        self.mode = mode
         self.sample = sample  # TODO: расширить sample: top_k = 10, p = 0.8
-        self.semi_supervised_df = None
 
     @staticmethod
     def process(df: pd.DataFrame, lm_params: Optional[Dict[str, Any]] = None) -> pd.DataFrame:
@@ -43,13 +48,13 @@ class UnsupervisedBaseline:
         pd.DataFrame
             Processed with GPT-2 dataset consisting of human/machine - continued paragraphs.
         """
-        unsupervised_data = []
+        labeled_data = []
         paragraphs = df.values.reshape(-1,).tolist()
         paragraphs = list(filter(check_input_paragraph, paragraphs))
         generated_paragraphs = generate_language_model(paragraphs, lm_params=lm_params)
         for i, generated_paragraph in tqdm.tqdm(enumerate(generated_paragraphs)):
             if check_output_paragraph(generated_paragraph):
-                unsupervised_data.extend([
+                labeled_data.extend([
                     {
                         "text": trim_output_paragraph(generated_paragraph),
                         "target": "machine"
@@ -59,12 +64,13 @@ class UnsupervisedBaseline:
                         "target": "human"
                     }
                 ])
-        unsupervised_df = pd.DataFrame(data=unsupervised_data, columns=["text", "target"])
-        return unsupervised_df
+        labeled_df = pd.DataFrame(data=labeled_data, columns=["text", "target"])
+        return labeled_df
 
     @staticmethod
     def label_with_repeats(
         df: pd.DataFrame,
+        target_name: str = "target",
         save_repeats: bool = False,
         collection_length: int = LM_LENGTH_LOWER_BOUND,
         smr_length: int = SMR_LENGTH_LOWER_BOUND,
@@ -72,7 +78,25 @@ class UnsupervisedBaseline:
         seed: int = 42
     ) -> pd.DataFrame:
         """
-        TODO-Docs
+        Label the dataset based on the algorithm provided in https://arxiv.org/abs/2111.02878.
+        We use suffix trees for super-maximal repeats counting.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            TODO-Docs
+        target_name: str
+        save_repeats: bool
+        collection_length: int
+        smr_length: int
+        positive_repeat_rate: float
+        seed: int
+            Random seed.
+
+        Returns
+        -------
+        pd.DataFrame
+            Labeled dataframe for the further unsupervised baseline training process.
         """
         parapgraphs_list = df["text"].values.tolist()
         if save_repeats:
@@ -87,27 +111,35 @@ class UnsupervisedBaseline:
             paragraph = row["text"]
             is_positive = any([repeat in paragraph for repeat in positive_repeats])
             smr_labels.append("machine" if is_positive else "human")
-        df["target"] = np.array(smr_labels)
+        df[target_name] = np.array(smr_labels)
         return df
 
     @staticmethod
     def semi_supervise(
         df: pd.DataFrame,
+        target_name: str = "target",
         supervise_rate: float = SEMI_SUPERVISED_HUMAN_RATE,
         seed: int = 42
     ) -> pd.DataFrame:
+        """
+        TODO-Docs
+        """
         supervised_sample = df.head(int(supervise_rate * len(df)))
-        supervised_sample = supervised_sample[supervised_sample["target"] == "human"]
+        supervised_sample = supervised_sample[supervised_sample[target_name] == "human"]
         unsupervised_sample = df.tail(int((1.0 - supervise_rate) * len(df)))
-        unsupervised_sample = UnsupervisedBaseline.label_with_repeats(unsupervised_sample, save_repeats=True, seed=seed)
+        unsupervised_sample = UnsupervisedBaseline.label_with_repeats(unsupervised_sample, target_name=target_name, save_repeats=True, seed=seed)
         return supervised_sample.append(unsupervised_sample)
 
-    def fit(self, df: pd.DataFrame, force: bool = False):
+    def fit(self, df: pd.DataFrame, target_name: str = "target", force: bool = False):
+        """
+        TODO-Docs
+        """
         # TODO: обобщить до двух опций: unsupervised и semi_supervised
-        if (not self.semi_supervised_df) or (self.semi_supervised_df and force):
+        if (not self.labeled_df) or (self.labeled_df and force):
+            # Here is target_name is set to default in order to distinguish labeled target and the ground truth one.
             df = self.process(df)
-            self.semi_supervised_df = UnsupervisedBaseline.semi_supervise(df)
-        X, y = self.semi_supervised_df["text"], self.semi_supervised_df["target"]
+            self.labeled_df = UnsupervisedBaseline.semi_supervise(df, target_name=target_name)
+        X, y = self.semi_supervised_df["text"], self.semi_supervised_df[target_name]
         self.detector.fit(X, y)
 
     def predict(self, X_test: pd.DataFrame):
@@ -120,7 +152,7 @@ if __name__ == "__main__":
     dvc_path = path.join(dir_path, "resources/data")
     news_df = pd.read_csv(path.join(dvc_path, "lenta/lenta_sample.csv"), nrows=20)
 
-    args = form_args()
-    baseline = UnsupervisedBaseline(args=args)
+    main_args = form_args()
+    baseline = UnsupervisedBaseline(args=main_args)
     baseline.fit(news_df)
     pass
