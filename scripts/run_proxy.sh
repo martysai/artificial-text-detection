148d75818a30e090f7dd026c39363f0eac731ead
Marat Saidov <msaidov@sbdagroup.com>
2022-04-20 15:49:33 +0300

    changed argument usage in the script

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -2,5 +2,5 @@
 
 python artificial_detection/data/proxy.py \
     --df_path="$HOME/atd-data/metrics_checkpoint_merged_df.tsv" \
-    --metrics_names="{!$1}" \
+    --metrics_names="$1" \
     --output_path="$HOME/atd-data/collected_metrics.tsv"
