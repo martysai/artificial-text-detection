14fe09102edf228fd55941d4890453913378e041
Marat Saidov <msaidov@sbdagroup.com>
2022-04-21 00:10:54 +0300

    customized output name

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -4,4 +4,4 @@ python artificial_detection/data/proxy.py \
     --df_path="$HOME/atd-data/metrics_checkpoint_merged_df.tsv" \
     --metrics_names="$1" \
     --model_path="$2" \
-    --output_path="$HOME/atd-data/collected_metrics.tsv"
+    --output_path="$HOME/atd-data/collected_metrics_$1.tsv"
